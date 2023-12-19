1cea2db72e3e6cefe41e2df3a02f5433bcf8c401
Afza7 <114289348+Afza7@users.noreply.github.com>
2023-12-20 02:54:01 +0530

    #1 of quiz

--- initial_access/urls.py
+++ initial_access/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('q1/',views.quiz1, name='initial_access')
+    path('q1/',views.quiz1, name='initial_access'),
+    path('q2/',views.quiz2, name='priv_escalate'),
+    path('result/', views.result, name='result'),
 ]
