9346414658bfdeba4871d73c4d3876402f73aaef
Afza7 <114289348+Afza7@users.noreply.github.com>
2023-12-20 15:50:00 +0530

    aa

--- initial_access/admin.py
+++ initial_access/admin.py
@@ -3,8 +3,14 @@ from .models import initaccess
 from .models import privescalation
 from .models import credentialaccess
 from .models import commandcontrol
+from .models import execution
+from .models import collections
+from .models import impact
 # Register your models here.
 admin.site.register(initaccess)
 admin.site.register(privescalation)
 admin.site.register(credentialaccess)
 admin.site.register(commandcontrol)
+admin.site.register(execution)
+admin.site.register(collections)
+admin.site.register(impact)
--- initial_access/views.py
+++ initial_access/views.py
@@ -37,40 +37,79 @@ def quiz3(request):
 
 
 def result_init(request):
-    # Fetching questions from the model
-    questions = initaccess.objects.all()
+    if request.method == 'POST':
+        print(request.POST)
+        question = initaccess.objects.all()
+        score=0
+        wrong=0
+        correct=0
+        total=0
+        temp=total
+        for q in question:
+            question_value= request.POST.get(q.question)
+            option_A_value = 'option_A'  # Use the correct form field name
+            option_B_value = 'option_B'
+            # Check if the selected option matches the correct option
+            if option_A_value == question_value:
+                score+=temp
+                correct+=1
+            elif option_B_value == question_value:
+                score-=temp
+                wrong+=1
+            temp-=1
+            total+=temp
+        percent = ((score/total)*100)
+        keywords = [question.keywords for question in question]
 
-    # Extracting keywords from questions
-    keywords = [question.keywords for question in questions]
+        # Counting occurrences of each keyword
+        keyword_count = {}
+        for keyword_list in keywords:
+            for keyword in keyword_list.split(','):  # Assuming keywords are separated by comma
+                keyword_count[keyword.strip()] = keyword_count.get(keyword.strip(), 0) + 1
 
-    # Counting occurrences of each keyword
-    keyword_count = {}
-    for keyword_list in keywords:
-        for keyword in keyword_list.split(','):  # Assuming keywords are separated by comma
-            keyword_count[keyword.strip()] = keyword_count.get(keyword.strip(), 0) + 1
+        # Sorting the keywords by count
+        sorted_keywords = sorted(keyword_count.items(), key=lambda x: x[1], reverse=True)
 
-    # Sorting the keywords by count
-    sorted_keywords = sorted(keyword_count.items(), key=lambda x: x[1], reverse=True)
+        # Extracting data for the chart
+        top_keywords = [x[0] for x in sorted_keywords[:10]]  # Top 10 keywords
+        counts = [x[1] for x in sorted_keywords[:10]]
+        labels = ['Complied', 'Non-Complied']
+        explode = (0.1, 0)  # explode 1st slice
 
-    # Extracting data for the chart
-    top_keywords = [x[0] for x in sorted_keywords[:10]]  # Top 10 keywords
-    counts = [x[1] for x in sorted_keywords[:10]]  # Corresponding counts
+        fig1, ax1 = plt.subplots(1, 2, figsize=(12, 6))
+        sizes = [correct, total - correct]
 
-    # Creating the bar chart
-    plt.figure(figsize=(10, 6))
-    plt.barh(top_keywords, counts)
-    plt.xlabel('Count')
+        sizes = np.nan_to_num(sizes)
+        # Pie chart
+        ax1[0].pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%', shadow=True, startangle=90)
 
-    # Saving the chart to a BytesIO object
-    buffer = BytesIO()
-    plt.savefig(buffer, format='png')
-    buffer.seek(0)
-    chart_image = base64.b64encode(buffer.getvalue()).decode('utf-8')
-    buffer.close()
+        # Bar chart
+        ax1[1].barh(top_keywords, counts)
+        ax1[1].set_xlabel('Count')
 
-    # Passing the chart image and other data to the template
-    context = {
-        'chart_image': chart_image,
-    }
-    return render(request, 'initial_access/result_init.html', context)
+        # Save the chart to a BytesIO buffer
+        chart_buffer = BytesIO()
+        plt.savefig(chart_buffer, format='png')
+        chart_buffer.seek(0)
+        chart_data = base64.b64encode(chart_buffer.getvalue()).decode('utf-8')
+        plt.close()
+
+        context = {
+            'score': score,
+            'correct': correct,
+            'wrong': wrong,
+            'percent': percent,
+            'total': total,
+            'chart_data': chart_data,
+            #'chart_image': chart_image,
+        }
+
+
+        return render(request,'initial_access/result_init.html',context)
+    else:
+        question=initaccess.objects.all()
+        context = {
+            'question':question
+        }
+        return render(request,'initial_access/result_init.html',context)
 
