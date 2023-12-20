7dfffecc1c391a30bab7618be543f2ce6b11f749
christina joseph jyothula <christinajosephjyothula@gmail.com>
2023-12-20 15:45:33 +0530

    .

--- initial_access/models.py
+++ initial_access/models.py
@@ -40,13 +40,71 @@ class commandcontrol(models.Model):
 
     def __str__(self):
         return self.question_id
-    
-class responses(models.Model):
-    session_id = models.IntegerField()
-    initial_access_result = models.IntegerField()
-    privescalation_result = models.IntegerField()
-    credentialaccess_result = models.IntegerField()
-    commandcontrol_result = models.IntegerField()
+
+class commandcontrol(models.Model):
+    question_id = models.CharField(max_length=200)
+    question=models.TextField(blank=True)
+    option_A=models.CharField(max_length=200,default='yes')
+    option_B=models.CharField(max_length=200,default='no')
+    keywords=models.TextField(blank=True)
 
     def __str__(self):
-        return self.session_id
+        return self.question_id
+
+class commandcontrol(models.Model):
+    question_id = models.CharField(max_length=200)
+    question=models.TextField(blank=True)
+    option_A=models.CharField(max_length=200,default='yes')
+    option_B=models.CharField(max_length=200,default='no')
+    keywords=models.TextField(blank=True)
+
+    def __str__(self):
+        return self.question_id
+
+class commandcontrol(models.Model):
+    question_id = models.CharField(max_length=200)
+    question=models.TextField(blank=True)
+    option_A=models.CharField(max_length=200,default='yes')
+    option_B=models.CharField(max_length=200,default='no')
+    keywords=models.TextField(blank=True)
+
+    def __str__(self):
+        return self.question_id
+    
+class execution(models.Model):
+    question_id = models.CharField(max_length=200)
+    question=models.TextField(blank=True)
+    option_A=models.CharField(max_length=200,default='yes')
+    option_B=models.CharField(max_length=200,default='no')
+    keywords=models.TextField(blank=True)
+    def __str__(self):
+        return self.question_id
+class collections(models.Model):
+    question_id = models.CharField(max_length=200)
+    question=models.TextField(blank=True)
+    option_A=models.CharField(max_length=200,default='yes')
+    option_B=models.CharField(max_length=200,default='no')
+    keywords=models.TextField(blank=True)
+    def __str__(self):
+        return self.question_id
+class impact(models.Model):
+    question_id = models.CharField(max_length=200)
+    question=models.TextField(blank=True)
+    option_A=models.CharField(max_length=200,default='yes')
+    option_B=models.CharField(max_length=200,default='no')
+    keywords=models.TextField(blank=True)
+    def __str__(self):
+        return self.question_id
+
+
+
+
+# class responses(models.Model):
+#     session_id = models.IntegerField()
+#     initial_access_result = models.IntegerField()
+#     privescalation_result = models.IntegerField()
+#     credentialaccess_result = models.IntegerField()
+#     commandcontrol_result = models.IntegerField()
+
+#     def __str__(self):
+#         return self.session_id
