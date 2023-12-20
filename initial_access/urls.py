9c761ac053f27728c52d5de7189a264a1fb3ac94
BabyVijayaShree <112560474+BabyVijayaShree@users.noreply.github.com>
2023-12-20 15:06:17 +0530

    .

--- initial_access/urls.py
+++ initial_access/urls.py
@@ -5,6 +5,5 @@ urlpatterns = [
     path('q1/',views.q1, name='initial_access'),
     path('q2/',views.q2, name='priv_escalate'),
     path('result/', views.result, name='result'),
-    path('startquiz',views.start_quiz,name="start_quiz"),
-    path('result/index.html',views.index,name='index'),
+    path('startquiz',views.start_quiz,name="start_quiz")
 ]
