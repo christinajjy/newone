ab866e45331b8020d8b91e084beefd95ee35b244
Afza7 <114289348+Afza7@users.noreply.github.com>
2023-12-20 15:43:07 +0530

    #7

--- initial_access/urls.py
+++ initial_access/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('modules/',views.modules, name='modules'),
     path('q1/',views.quiz1, name='initial_access'),
     path('q2/',views.quiz2, name='priv_escalate'),
-    path('q3', views.quiz3, name='commandcontrol'),
+    path('q3/', views.quiz3, name='commandcontrol'),
     path('result_init/', views.result_init, name='result_init'),
     #path('startquiz',views.start_quiz,name="start_quiz"),
 
